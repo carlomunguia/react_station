9b039d4c397b95acd844c0dc645414850d53c4dc
Carlo Munguia <carlo.munguia.82@gmail.com>
2021-03-03 21:42:25 -0600

    working through react-components

---
+++ Basic_Component/index.js
@@ -0,0 +1,14 @@
+class Hello extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello There!</h1>
+                <h1>Hello There!</h1>
+                <h1>Hello There!</h1>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Hello />, document.getElementById('root'))
+
