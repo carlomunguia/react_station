561a78aad63cea8f32657a513adbc46052e7aa36
Carlo Munguia <carlo.munguia.82@gmail.com>
2021-03-06 10:35:00 -0600

    general wiring of pre-cli app creation

---
+++ App_Structure/Hello.js
@@ -0,0 +1,11 @@
+class Hello extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    hi there!
+                </h1>
+            </div>
+        )
+    }
+}
