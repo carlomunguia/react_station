458f563300771ac8df3f4765375fccdebe1c12b5
Carlo Munguia <carlo.munguia.82@gmail.com>
2021-03-07 22:45:38 -0600

    created todo component, created material infrastructure for todo, now to edit/delete functionality

--- captain-hook/src/TodoApp.js
+++ captain-hook/src/TodoApp.js
@@ -5,6 +5,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Grid from '@material-ui/core/Grid';
 import TodoList from './TodoList';
+import TodoForm from './TodoForm';
 
 function TodoApp() {
   const initialTodos = [
@@ -13,6 +14,9 @@ function TodoApp() {
     { id: 3, task: 'Wash Glass Jars', completed: false },
   ];
   const [todos, setTodos] = useState(initialTodos);
+  const addTodo = (newTodoText) => {
+    setTodos([...todos, { id: 4, task: newTodoText, completed: false }]);
+  };
 
   return (
     <Paper
@@ -26,10 +30,15 @@ function TodoApp() {
     >
       <AppBar color="primary" position="static" style={{ height: '64px' }}>
         <Toolbar>
-          <Typography color="inherit">HOOKY TODOS</Typography>
+          <Typography color="inherit">HOOK-E'S TODO LIST!</Typography>
         </Toolbar>
       </AppBar>
-      <TodoList todos={todos} />
+      <Grid container justify="center" style={{ marginTop: '1rem' }}>
+        <Grid item xs={11} md={8} lg={4}>
+          <TodoForm addTodo={addTodo} />
+          <TodoList todos={todos} />
+        </Grid>
+      </Grid>
     </Paper>
   );
 }
--- captain-hook/src/TodoList.js
+++ captain-hook/src/TodoList.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
+
 import Divider from '@material-ui/core/Divider';
+import Todo from './Todo';
 
 function TodoList(props) {
   return (
@@ -11,11 +11,7 @@ function TodoList(props) {
       <List>
         {props.todos.map((todo) => (
           <>
-            <ListItem>
-              <ListItemText>
-                <li>{todo.task}</li>
-              </ListItemText>
-            </ListItem>
+            <Todo task={todo.task} key={todo.id} completed={todo.completed} />
             <Divider />
           </>
         ))}
